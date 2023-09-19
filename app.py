8da36b50fdfc256e2bbadd54e5e3fb2eba85bf17
seoyeon22 <hsy2709@gmail.com>
2023-09-19 16:35:55 +0900

    modify join_chatroom

--- app.py
+++ app.py
@@ -23,9 +23,9 @@ app.config['ALLOWED_EXTENSIONS'] = {'jpg', 'jpeg', 'png'}
 socketio = SocketIO(app)
 
 # 각자 데이터베이스에 맞춰서 변경 
-db = pymysql.connect(host='localhost', user='root', password='2023', db='overtheglass')
+# db = pymysql.connect(host='localhost', user='root', password='2023', db='overtheglass')
 # db = pymysql.connect(host='localhost', user='root', password='0717', db='overtheglass')
-#db = pymysql.connect(host='localhost', user='root', password='0000', db='overtheglass')
+db = pymysql.connect(host='localhost', user='root', password='0000', db='overtheglass')
 
 m = hashlib.sha256()
 m.update('Over the Glass'.encode('utf-8'))
